be34e8913893f3fd4734f9d6b8a28a88bbfadbbe
phucnn1707 <19520224@gm.uit.edu.vn>
2024-06-19 09:34:03 +0700

    change voice

--- server/src/services/gpt.js
+++ server/src/services/gpt.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 const OpenAI = require('openai');
 const path = require('path');
+const ElevenLabs = require('elevenlabs-node');
 
 require('dotenv').config({ path: '.env.example' });
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
+});
+const apiKeyElevenLabs = process.env.ELEVEN_LABS_API_KEY;
+const voiceID = 'wJqPPQ618aTW29mptyoc';
+
+const voice = new ElevenLabs({
+    apiKey: apiKeyElevenLabs, // Your API key from Elevenlabs
+    voiceId: voiceID // A Voice ID from Elevenlabs
 });
 
 const speechFile = path.join(__dirname, '../../storage/tmp', 'Response.mp3');
@@ -37,21 +45,29 @@ exports.Speech2Speech = async(req, res) => {
 
         console.log(response.choices[0].message.content);
 
+        //const test = await voice.textToSpeechStream(apiKeyElevenLabs, voiceID, response.choices[0].message.content);
+        const test = await voice.textToSpeechStream({
+            textInput: response.choices[0].message.content,
+            voiceId: 'wJqPPQ618aTW29mptyoc'
+        });
+
         const mp3 = await openai.audio.speech.create({
             model: 'tts-1',
             voice: 'nova',
             input: response.choices[0].message.content
         });
 
-        const buffer = Buffer.from(await mp3.arrayBuffer());
-        await fs.promises.writeFile(speechFile, buffer);
+        test.pipe(fs.createWriteStream(speechFile));
 
-        const base64Data = buffer.toString('base64');
+        //const buffer = Buffer.from(await test.arrayBuffer());
+        //await fs.promises.writeFile(speechFile, buffer);
+
+        //const base64Data = buffer.toString('base64');
 
         return {
             transcription: transcription.text,
-            response: response.choices[0].message.content,
-            chatgptAudio: base64Data
+            response: response.choices[0].message.content
+                //chatgptAudio: base64Data
         };
     } catch (error) {
         console.error('Error during transcription:', error);
